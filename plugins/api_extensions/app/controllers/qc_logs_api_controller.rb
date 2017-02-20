bcdfcf297c45ff9b7a1c4eab866f336362a00e04
Aaron <anagucki@chepri.com>
2017-02-20 17:47:06 -0500

    [BPRM] create log

--- plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
+++ plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
@@ -52,35 +52,19 @@ class QcLogsApiController < ApplicationController
   #   @project.safe_attributes = params[:project]
   # end
 
-  # def create
-  #   @issue_custom_fields = IssueCustomField.sorted.to_a
-  #   @trackers = Tracker.sorted.to_a
-  #   @project = Project.new
-  #   @project.safe_attributes = params[:project]
+  def create
 
-  #   if @project.save
-  #     unless User.current.admin?
-  #       @project.add_default_member(User.current)
-  #     end
-  #     respond_to do |format|
-  #       format.html {
-  #         flash[:notice] = l(:notice_successful_create)
-  #         if params[:continue]
-  #           attrs = {:parent_id => @project.parent_id}.reject {|k,v| v.nil?}
-  #           redirect_to new_project_path(attrs)
-  #         else
-  #           redirect_to settings_project_path(@project)
-  #         end
-  #       }
-  #       format.api  { render :action => 'show', :status => :created, :location => url_for(:controller => 'projects', :action => 'show', :id => @project.id) }
-  #     end
-  #   else
-  #     respond_to do |format|
-  #       format.html { render :action => 'new' }
-  #       format.api  { render_validation_errors(@project) }
-  #     end
-  #   end
-  # end
+    @qc_log = QcLog.new(params[:qc_log])
+
+    if @qc_log.save
+      unless User.current.admin?
+        @qc_log.add_default_member(User.current)
+      end
+      render json:  { qc_log: @qc_log }
+    else
+      render json:  { qc_log: render_validation_errors(@qc_log) }
+    end
+  end
 
   # def copy
   #   @issue_custom_fields = IssueCustomField.sorted.to_a
--- plugins/api_extensions/config/routes.rb
+++ plugins/api_extensions/config/routes.rb
@@ -2,3 +2,4 @@
 # See: http://guides.rubyonrails.org/routing.html
 get 'projects_api/projects', to: 'projects_api#index'
 get '/qc_logs_api/qc_logs', to: 'qc_logs_api#index'
+post 'qc_logs_api/qc_logs', to: 'qc_logs_api#create'
