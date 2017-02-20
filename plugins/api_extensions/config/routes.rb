88e66239972a6bb7c42fbf8ff9d2ca0fca65ec68
Aaron <anagucki@chepri.com>
2017-02-20 15:12:23 -0500

    [BPRM] Got this stuff(qc_log) going

--- plugins/api_extensions/config/routes.rb
+++ plugins/api_extensions/config/routes.rb
@@ -1,3 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get 'projects_api/projects', to: 'projects_api#index'
+get '/qc_logs_api/qc_logs', to: 'qc_logs_api#index'
--- plugins/qc_logs/app/models/qc_log.rb
+++ plugins/qc_logs/app/models/qc_log.rb
@@ -2,4 +2,6 @@ class QcLog < ActiveRecord::Base
   unloadable
   belongs_to :project
   belongs_to :user
+  scope :visible, lambda {|*args| joins(:project).where(Project.allowed_to_condition(args.shift || User.current, :view_qc_logs)) }
+
 end
