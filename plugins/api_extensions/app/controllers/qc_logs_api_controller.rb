7c0c57509d35904e1de8853e3c94a3f6ef4be3d0
Aaron <anagucki@chepri.com>
2017-02-22 17:48:38 -0500

    [BPRM] update update

--- plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
+++ plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
@@ -95,6 +95,7 @@ class QcLogsApiController < ApplicationController
       render json: {}, status: :no_content
     rescue ActiveRecord::RecordNotFound
       render :json => {}, :status => :not_found
+    end
   end
 
   # def copy
