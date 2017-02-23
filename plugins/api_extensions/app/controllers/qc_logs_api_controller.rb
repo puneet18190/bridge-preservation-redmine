e225136fe57e8e0388cbba6de362c48baef7dc22
Jjnagy <jnagy@chepri.com>
2017-02-23 18:52:34 -0500

    [BP] Fixing an end point issue for logs.

--- plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
+++ plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
@@ -112,7 +112,7 @@ class QcLogsApiController < ApplicationController
   private
 
   def search_filter(scope)
-    filter_params = JSON.parse(params[:filters]) rescue {}
+    filter_params = params[:filters]
     scope = scope.has_project_with_id(params[:project_id]) if params[:project_id]
     scope = scope.text_search(filter_params) if filter_params.is_a?(Hash)
     return scope
