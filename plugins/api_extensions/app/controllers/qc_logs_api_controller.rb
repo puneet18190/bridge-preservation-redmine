3b6e9f39e18b43d189c85cfc0408c4fea7b1d81f
Damien Pyles <dpyles@chepri.com>
2017-02-23 14:28:40 -0500

    [BPR] added check for params

--- plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
+++ plugins/api_extensions/app/controllers/qc_logs_api_controller.rb
@@ -112,8 +112,8 @@ class QcLogsApiController < ApplicationController
   private
 
   def search_filter(scope)
-    filter_params = JSON.parse(params[:filters])
-    scope = scope.has_project_with_id(params[:project_id])
+    filter_params = JSON.parse(params[:filters]) rescue {}
+    scope = scope.has_project_with_id(params[:project_id]) if params[:project_id]
     scope = scope.text_search(filter_params) if filter_params.is_a?(Hash)
     return scope
 
