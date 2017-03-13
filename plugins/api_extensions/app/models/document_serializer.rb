88627a70fd2d93a17eaf664d646ec827379a0978
Damien Pyles <dpyles@Jeffs-Mac-Pro.local>
2017-03-13 18:46:05 -0400

    [BPR-59] added category name to document serializer

--- plugins/api_extensions/app/models/document_serializer.rb
+++ plugins/api_extensions/app/models/document_serializer.rb
@@ -1,11 +1,14 @@
 class DocumentSerializer < ActiveModel::Serializer
   unloadable
 
-  attributes *Document.attribute_names, :attachments
+  attributes *Document.attribute_names, :attachments, :category_name
   include Rails.application.routes.url_helpers
 
  #available event methods
  #[:event_options, :event_options=, :event_datetime, :event_title, :event_description, :event_author, :event_type, :event_date, :event_group, :event_url]
+ def category_name
+  object.category.name rescue ""
+ end
 
   def attachments
    object.attachments
