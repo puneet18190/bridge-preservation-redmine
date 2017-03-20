546f75683425d83066a10fa3b6d2a65a4d363d2e
vagrant <vagrant@vagrant.vm>
2017-03-20 18:14:40 -0400

    Adding a link back to front end from redmine.

--- plugins/qc_logs/init.rb
+++ plugins/qc_logs/init.rb
@@ -5,6 +5,7 @@ Redmine::Plugin.register :qc_logs do
   version '0.0.1'
   url 'http://www.Chepri.com/'
   author_url 'http://www.chepri.com/'
+  menu :top_menu, :front_end, "#{Redmine::Configuration['prototype_domain_path']}", :caption => "Front End", last: true
 
   menu :project_menu, :qc_logs, { controller: 'qc_logs', action: 'index' }, caption: 'QC Logs', last: true, param: :project_id
   require_dependency 'qc_logs_hook_listener'
